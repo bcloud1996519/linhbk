86fd7fb353e7b7750c88e26b1c6ed91836f99dc5
bcloud1996519 <tuanlinh.19.5.96@gmail.com>
2018-06-29 10:10:03 +0700

    nhanh1
    Rubocop

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+
+
+# frozen_string_literal: true
+#
+# Examples:
+#
+
+#   Character.create(name: 'Luke', movie: movies.first)
---
+++ test/application_system_test_case.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Xong Roi =))
+require "test_helper"
+# Xong roi lan 2
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+end
